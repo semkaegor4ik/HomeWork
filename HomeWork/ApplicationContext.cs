d707788203540f87fe7c428b43a721876a64e572
26790 <267902.egor@gmail.com>
2021-12-04 11:37:26 +0300

    Добавьте файлы проекта.

---
+++ HomeWork/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HomeWork
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=kirill;Username=postgres;Password=pass");
+        }
+    }
+}
---
+++ HomeWork/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Create();
+            Read();
+            Update();
+            Read();
+            Delete();
+            Read();
+        }
+
+        private static void Delete()
+        {
+            using(ApplicationContext db = new ApplicationContext())
+            {
+                db.Users.RemoveRange(db.Users.Where(user => user.Id % 2 == 1));
+                db.SaveChanges();
+            }
+        }
+
+        private static void Update()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+                users.ForEach(u => ++u.Age);
+                db.SaveChanges();
+            }
+        }
+
+        private static void Read()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.ToList();
+                Console.WriteLine("Users list:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+        }
+
+        private static void Create()
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.AddRange(user1, user2);
+                db.SaveChanges();
+            }
+        }
+    }
+}
